260b688029b8634e5417179b4fb0d3bcbeb12bf2
ADMIN <ADMIN@PC216>
2024-10-25 09:41:31 +0700

    pb2

--- HoangAnh/src/main/java/controller/ProductManagement.java
+++ HoangAnh/src/main/java/controller/ProductManagement.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package QLSP;
+package controller;
 
 import dao.HoaDAO;
 import dao.LoaiDAO;
@@ -24,7 +24,7 @@ import model.Hoa;
  * @author ADMIN
  */
 @MultipartConfig
-public class  extends HttpServlet {
+public class ProductManagement  extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
